8bf1f5bd18838586a3d6318904ebb8762a8b6605
Frank Petrilli <frank@petril.li>
2018-02-09 10:55:18 -0800

    Split into init/run containers, separate containers
    Splitting into different containers so k8s can do its job
    and restart containers which have failed. May separate into pods...
    
    Not sure if it's a better option to keep it self-contained, or allow for separate administration /
    scaling that a multipod solution would offer.

--- flow_collector/init.sh
+++ flow_collector/init.sh
@@ -1,28 +1,36 @@
 #!/usr/bin/env bash
 
-if [ -z $BULK_INSERT_COUNT ]
-then
-	BULK_INSERT_COUNT=700
-fi
+if [ "$1" = "init" ]; then
+	if [ -z $BULK_INSERT_COUNT ]
+	then
+		BULK_INSERT_COUNT=700
+	fi
 
-if [ -z $FLOW_ELASTICSEARCH_SERVICE_HOST ]
-then
-	FLOW_ELASTICSEARCH_SERVICE_HOST="localhost"
-fi
+	if [ -z $FLOW_ELASTICSEARCH_SERVICE_HOST ]
+	then
+		FLOW_ELASTICSEARCH_SERVICE_HOST="localhost"
+	fi
 	
-
-cat > /root/data/netflow_options.py << EOF
-bulk_insert_count = $BULK_INSERT_COUNT
-netflow_v5_port = 2055
-netflow_v9_port = 9995
-ipfix_port = 4739
-sflow_port = 6343
-elasticsearch_host = '$FLOW_ELASTICSEARCH_SERVICE_HOST'
-dns = True
-lookup_internal = False
-mac_lookup = True
+	cat > /root/data/netflow_options.py << EOF
+	bulk_insert_count = $BULK_INSERT_COUNT
+	netflow_v5_port = 2055
+	netflow_v9_port = 9995
+	ipfix_port = 4739
+	sflow_port = 6343
+	elasticsearch_host = '$FLOW_ELASTICSEARCH_SERVICE_HOST'
+	dns = True
+	lookup_internal = False
+	mac_lookup = True
 EOF
-/usr/bin/env python /root/data/ipfix.py &
-/usr/bin/env python /root/data/netflow_v5.py &
-/usr/bin/env python /root/data/netflow_v9.py &
+
+elif [ "$1" = "ipfix" ]; then
+/usr/bin/env python /root/data/ipfix.py
+elif [ "$1" = "netflow_v5" ]; then
+/usr/bin/env python /root/data/netflow_v5.py
+elif [ "$1" = "netflow_v9" ]; then
+/usr/bin/env python /root/data/netflow_v9.py
+elif [ "$1" = "sflow" ]; then
 /usr/bin/env python /root/data/sflow.py
+else 
+printf "Usage: %s {init, ipfix, netflow_v5, netflow_v9, sflow}\n" $0
+fi
